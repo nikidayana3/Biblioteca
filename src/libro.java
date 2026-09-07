7d4560688c8e9872456cd0a789592e0e44e37ab1
Nicol Dayana <nikidayan3@gmail.com>
2026-09-07 07:25:21 -0500

    se creo clase del libro

--- src/libro.java
+++ src/libro.java
@@ -1,2 +1,9 @@
 public class libro {
+    private String titulo;
+    private String autor;
+    private String codigo;
+    private String genero;
+    private int anioPublicacion;
+    private int copiasDisponible;
+
 }

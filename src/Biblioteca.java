12ec193819c946c4dc10e8d10d486f677cdc5cee
Nicol Dayana <nikidayan3@gmail.com>
2026-09-12 18:19:51 -0500

    Se agrego  HashMap, array y validaciones en agregarLibro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,12 +1,40 @@
 import java.util.ArrayList;
+import java.util.HashMap;
     public class Biblioteca {
         private ArrayList<Libro> libros;
+        private HashMap<String, ArrayList<Libro>>indiceAutor;
+        private String[] generosValidos = {"Novela", "Ciencia", "Historia", "Infantil", "Tecnico"};
 
         public Biblioteca() {
             libros= new ArrayList<Libro>();
+            indiceAutor=new HashMap<String, ArrayList<Libro>>();
         }
-        public void agregarLibro(Libro libro){
+        public boolean agregarLibro(Libro libro) {
+            if (libro.getTitulo() == null || libro.getTitulo().isEmpty()) {
+                return false;
+            }
+            if (libro.getAutor() == null || libro.getAutor().isEmpty()) {
+                return false;
+            }
+            if (libro.getCodigo() == null || libro.getCodigo().isEmpty()) {
+                return false;
+            }
+
+            for (Libro l : libros) {
+                if (l.getCodigo().equalsIgnoreCase(libro.getCodigo())) {
+                    return false;
+                }
+            }
+
             libros.add(libro);
+
+            String autor = libro.getAutor();
+            if (!indiceAutor.containsKey(autor)) {
+                indiceAutor.put(autor, new ArrayList<Libro>());
+            }
+            indiceAutor.get(autor).add(libro);
+
+            return true;
         }
         public ArrayList<Libro> obtenerTodos(){
             return libros;
@@ -23,5 +51,6 @@ import java.util.ArrayList;
             }
             return resultado;
         }
+
     }
 

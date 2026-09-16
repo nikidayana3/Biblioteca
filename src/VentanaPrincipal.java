9bfc0550bea513571909a72907729572832fc706
Nicol Dayana <nikidayan3@gmail.com>
2026-09-15 19:56:52 -0500

    mostrar tabla con libros

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -3,7 +3,7 @@ import javax.swing.table.DefaultTableModel;
 
     public class VentanaPrincipal extends JFrame {
 
-        public VentanaPrincipal() {
+        public VentanaPrincipal(Biblioteca biblioteca) {
             setTitle("Sistema de Gestión de Biblioteca");
             setSize(600, 400);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -11,6 +11,8 @@ import javax.swing.table.DefaultTableModel;
             DefaultTableModel modelo = new DefaultTableModel(columnas, 0);
             JTable tabla = new JTable(modelo);
             add(new JScrollPane(tabla));
+            for (Libro libro: biblioteca.obtenerTodos()){
+                modelo.addRow(new Object[]{libro.getTitulo(), libro.getAutor(), libro.getCodigo(), libro.getGenero(), libro.getAnioPublicacion(), libro.getCopiasDisponible()});}
 
             setVisible(true);
         }
